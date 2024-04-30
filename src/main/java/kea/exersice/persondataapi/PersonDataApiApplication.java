50ae609be1d0678211da787d196d50c1ef379463
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-04-30 10:28:38 +0200

    Initial Commit

---
+++ src/main/java/kea/exersice/persondataapi/PersonDataApiApplication.java
@@ -0,0 +1,13 @@
+package kea.exersice.persondataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonDataApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/exersice/persondataapi/PersonDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.exersice.persondataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonDataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
